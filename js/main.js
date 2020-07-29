f0b86d31e0a340f1f340366afa01345e040f09a9
shaksper <liuxu_net@126.com>
2020-07-29 19:33:05 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@
 
 	if($('#days').length > 0 ) {
 	 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 28, 2017 15:37:25").getTime();
+		var countDownDate = new Date("Feb 7, 2021 00:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
